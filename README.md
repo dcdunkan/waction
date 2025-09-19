fd1c252560495d06b593f6541d5fc5514547da78
Dunkan <70066170+dcdunkan@users.noreply.github.com>
2025-09-20 00:54:17 +0530

    initial commit: basic canvas based 2d game engine with stage & actor

---
+++ README.md
@@ -0,0 +1,12 @@
+> Heavily work-in-progress. This does nothing right now.
+
+# waction!!
+
+> AI was NOT used for the making of this game.
+
+Ever wanted to fight with your contacts and group chats on WhatsApp?
+waction is a userscript based game that runs on WhatsApp web app, where you can shoot and
+kill your contacts and destroy the bossy and mega-bossy group chats.
+
+Made for Hackade: Game Hackathon by CSI TKMCE.
+
---
+++ waction.js
@@ -0,0 +1,228 @@
+(() => {
+	if (window.lastFrame != null) {
+		cancelAnimationFrame(window.lastFrame);
+	}
+
+	// constants
+	const SVG_ICONS = {
+		play: `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-play-icon lucide-play"><path d="M5 5a2 2 0 0 1 3.008-1.728l11.997 6.998a2 2 0 0 1 .003 3.458l-12 7A2 2 0 0 1 5 19z"/></svg>`,
+		pause: `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-pause-icon lucide-pause"><rect x="14" y="3" width="5" height="18" rx="1"/><rect x="5" y="3" width="5" height="18" rx="1"/></svg>`,
+		stop: `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-square-icon lucide-square"><rect width="18" height="18" x="3" y="3" rx="2"/></svg>`
+	};
+	const CANVAS_BACKGROUND_COLOR = "#fff";
+	const GAME_STATES = {
+		PLAYING: 1,
+		PAUSED: 2,
+		STOPPED: 3
+	}
+	const MILLISECOND = 1,
+		SECOND = 1000 * MILLISECOND,
+		MINUTE = 60 * SECOND,
+		HOUR = 60 * MINUTE;
+
+	// globals
+	window.lastFrame = null;
+	window.gameState = GAME_STATES.STOPPED;
+
+	// classes
+	class Actor {
+		x = 0;
+		y = 0;
+		visible = true;
+		stage = null;
+
+		constructor() {
+			this.x = 0;
+			this.y = 0;
+			this.visible = true;
+			this.stage = null;
+		}
+
+		setStage(stage) {
+			this.stage = stage;
+		}
+
+		act(delta) {}
+		draw() {}
+	}
+
+	class ImageActor extends Actor {
+		img = new Image();
+		
+		constructor() {
+			super();
+			
+			this.visible = false;
+			
+			this.img.onload = () => {
+				this.visible = true;
+			}
+		}
+		
+		act(delta) {
+			super.act(delta);
+			
+			let dx = delta * 30;
+			this.x += dx;
+			this.y += dx;
+		}
+		
+		draw() {
+			super.draw();
+			this.stage.ctx.drawImage(this.img, this.x, this.y);
+		}
+	}
+
+	class Stage {
+		canvas;
+		ctx;
+		#actors;
+
+		constructor(canvas) {
+			this.canvas = canvas;
+			this.ctx = canvas.getContext("2d");
+			this.#actors = [];
+		}
+
+		addActor(actor) {
+			this.#actors.push(actor);
+			actor.setStage(this);
+		}
+
+		act(delta) {
+			for (const actor of this.#actors) {
+				actor.act(delta);
+			}
+		}
+
+		draw() {
+			this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+			for (const actor of this.#actors) {
+				if (actor.visible) {
+					actor.draw();
+				}
+			}
+		}
+	}
+
+	function setupGameArea() {
+		const app = document.getElementById("app");
+		const leftPanel = document.getElementById("side");
+		const rightPanel = leftPanel.parentElement.nextSibling;
+		while (rightPanel.lastElementChild) { // cleanup
+			rightPanel.removeChild(rightPanel.lastElementChild);
+		}
+
+		const canvas = document.createElement("canvas");
+		canvas.id = "game-canvas";
+		canvas.style.position = "fixed";
+		canvas.style.width = "100%";
+		canvas.style.height = "100%";
+		canvas.style.backgroundColor = CANVAS_BACKGROUND_COLOR;
+		rightPanel.appendChild(canvas);
+
+		const sidebar = leftPanel.parentElement.previousSibling.previousSibling;
+		const settingsButton = sidebar.querySelector("[aria-label='Settings']").parentElement;
+		const bottomSidebarSection = settingsButton.parentElement;
+		while (bottomSidebarSection.children.length > 2) { // cleanup
+			bottomSidebarSection.removeChild(bottomSidebarSection.children[0]);
+		}
+		const gameControlButton = settingsButton.cloneNode(true);
+		bottomSidebarSection.prepend(gameControlButton);
+		gameControlButton.id = "wac-control-button";
+	}
+
+	// starting the game
+	setupGameArea();
+
+	const controlButton = document.getElementById("wac-control-button");
+	controlButton.firstChild.onclick = play;
+	changeControlButtonIcon(SVG_ICONS.play);
+
+	function changeControlButtonIcon(svgIcon) {
+		controlButton.firstChild.querySelector("span").innerHTML = svgIcon;
+	}
+
+	function initialize() {
+		let lastTimestamp = null;
+		let frames = 0;
+		let secondProgress = 0;
+
+		const canvas = document.getElementById("game-canvas");
+		const stage = new Stage(canvas);
+
+		const actor = new ImageActor();
+		actor.img.src = "https://pps.whatsapp.net/v/t61.24694-24/144532566_122644909725307_1437525764302012490_n.jpg?stp=dst-jpg_s96x96_tt6&ccb=11-4&oh=01_Q5Aa2gEE1iuUGKdL-T7UC59rlqof0ygj_MPGt59HV6NAWlPbLw&oe=68DAAD7F&_nc_sid=5e03e0&_nc_cat=102";
+		stage.addActor(actor);
+
+		function loop(frameTimestamp) {
+			if (lastTimestamp == null) {
+				lastTimestamp = frameTimestamp;
+			}
+			const delta = (frameTimestamp - lastTimestamp) / SECOND;
+			lastTimestamp = frameTimestamp;
+
+			frames++;
+			secondProgress += delta;
+			if (secondProgress >= 1) {
+				secondProgress = 0;
+				console.log("FPS:", frames);
+				frames = 0;
+			}
+
+			stage.act(delta);
+			stage.draw();
+			window.lastFrame = requestAnimationFrame(loop);
+		}
+		window.lastFrame = requestAnimationFrame(loop);
+	}
+
+	function play() {
+		window.gameState = GAME_STATES.PLAYING;
+		console.info("PLAYING");
+		controlButton.firstChild.onclick = stop;
+		changeControlButtonIcon(SVG_ICONS.stop);
+
+		initialize();
+	};
+
+	function stop() {
+		window.gameState = GAME_STATES.STOPPED;
+		console.info("STOPPING");
+		controlButton.firstChild.onclick = play;
+		changeControlButtonIcon(SVG_ICONS.play);
+
+		if (window.lastFrame == null) {
+			console.error("No game to stop??!");
+			return;
+		}
+		cancelAnimationFrame(window.lastFrame);
+	}
+})();
+
+function setupData() {
+	return new Promise((resolve, reject) => {
+		const dbOpenRequest = indexedDB.open("model-storage");
+		dbOpenRequest.onerror = (event) => {
+			console.log(event)
+			console.error("Something went wrong")
+			reject();
+		}
+		dbOpenRequest.onsuccess = async () => {
+			const db = dbOpenRequest.result;
+			const transaction = db.transaction(db.objectStoreNames, "readonly");
+			const chats = await getStoreData(transaction, "chat");
+			const profilePicThumbs = await getStoreData(transaction, "profile-pic-thumb");
+			// const messages = await getStoreData(transaction, "message");
+			resolve({ chats, profilePicThumbs });
+		}
+	})
+};
+
+function getStoreData(transaction, storeName) {
+	return new Promise((resolve, reject) => {
+		const request = transaction.objectStore(storeName).getAll();
+		request.onerror = () => reject();
+		request.onsuccess = () => resolve(request.result);
+	})
+}
