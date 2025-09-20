6097522d4513a9233a3f2edfd9f0820f5dcb0011
Dunkan <70066170+dcdunkan@users.noreply.github.com>
2025-09-21 02:05:12 +0530

    add shooting and reset game area each time

--- waction.js
+++ waction.js
@@ -2,7 +2,7 @@ async function run() {
 	if (window.lastFrame != null) {
 		cancelAnimationFrame(window.lastFrame);
 	}
-	
+
 	if (window.restoreListeners) {
 		window.restoreListeners();
 	}
@@ -39,20 +39,31 @@ async function run() {
 
 	let currentKey = {};
 	let currentKeyTime = {};
+	let currentMouse = null;
+	let currentMouseX = null;
+	let currentMouseY = null;
 
 	const CHARACTER_SIZE = 64;
 
+	function randomStr(length) {
+		const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+		let result = "";
+		const randomArray = new Uint8Array(length);
+		crypto.getRandomValues(randomArray);
+		randomArray.forEach((number) => {
+			result += chars[number % chars.length];
+		});
+		return result;
+	}
+
+
 	// classes
-	class Actor {
-		x = 0;
-		y = 0;
-		visible = true;
+	class BaseActor {
+		id = "";
 		stage = null;
 
 		constructor() {
-			this.x = 0;
-			this.y = 0;
-			this.visible = true;
+			this.id = "";
 			this.stage = null;
 		}
 
@@ -60,13 +71,38 @@ async function run() {
 			this.stage = stage;
 		}
 
+		act(delta) {}
+		draw() {}
+	}
+
+	class Actor extends BaseActor {
+		x = 0;
+		y = 0;
+		width = 0;
+		height = 0;
+		visible = true;
+
+		constructor() {
+			super();
+			this.x = 0;
+			this.y = 0;
+			this.visible = true;
+			this.width = 0;
+			this.height = 0;
+		}
+
 		setPosition(x, y) {
 			this.x = x;
 			this.y = y;
 		}
 
-		act(delta) {}
-		draw() {}
+		act(delta) {
+			super.act(delta);
+		}
+
+		draw() {
+			super.draw();
+		}
 	}
 
 	class ChatActor extends Actor {
@@ -78,6 +114,8 @@ async function run() {
 			super();
 
 			this.visible = false;
+			this.width = CHARACTER_SIZE;
+			this.height = CHARACTER_SIZE;
 
 			this.img.onload = () => {
 				this.visible = true;
@@ -108,7 +146,7 @@ async function run() {
 
 			if (this.chat == null) return;
 
-			const radius = CHARACTER_SIZE / 2;
+			const radius = this.width / 2;
 
 			const ctx = this.stage.ctx;
 
@@ -116,13 +154,13 @@ async function run() {
 			ctx.beginPath();
 			ctx.arc(this.x + radius, this.y + radius, radius, 0, Math.PI * 2);
 			ctx.clip();
-			ctx.drawImage(this.img, this.x, this.y, CHARACTER_SIZE, CHARACTER_SIZE);
+			ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
 			ctx.restore();
 
 			ctx.font = "16px 'Arial'";
 			ctx.textAlign = "center";
 			ctx.fillStyle = "#fff";
-			ctx.fillText(this.chatName, this.x + radius, this.y + CHARACTER_SIZE + 20);
+			ctx.fillText(this.chatName, this.x + radius, this.y + this.height + 20);
 		}
 	}
 
@@ -146,8 +184,23 @@ async function run() {
 		}
 
 		addActor(actor) {
+			let id;
+			do {
+				id = randomStr(24);
+			} while (this.#actors.some((a) => a.id === id));
+			actor.id = id;
 			this.#actors.push(actor);
 			actor.setStage(this);
+		}
+
+		removeActor(actor) {
+			const index = this.#actors.findIndex((a) => a.id === actor.id);
+			const [removedActor] = this.#actors.splice(index, 1);
+			removedActor.setStage(null);
+		}
+
+		getActors() {
+			return this.#actors;
 		}
 
 		act(delta) {
@@ -157,33 +210,18 @@ async function run() {
 		}
 
 		draw() {
+			this.ctx.resetTransform();
 			this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 			for (const actor of this.#actors) {
-				if (actor.visible) {
+				if (actor.visible === true) {
 					actor.draw();
 				}
 			}
 		}
 	}
 
-	function setupGameArea() {
-		const app = document.getElementById("app");
+	function setupControlButtons() {
 		const leftPanel = document.getElementById("side");
-		const rightPanel = leftPanel.parentElement.nextSibling;
-		while (rightPanel.lastElementChild) { // cleanup
-			rightPanel.removeChild(rightPanel.lastElementChild);
-		}
-
-		const canvas = document.createElement("canvas");
-		rightPanel.appendChild(canvas);
-
-		canvas.id = "game-canvas";
-		canvas.style.position = "fixed";
-		canvas.style.width = "100%";
-		canvas.style.height = "100%";
-		canvas.style.backgroundColor = CANVAS_BACKGROUND_COLOR;
-		canvas.width = canvas.clientWidth;
-		canvas.height = canvas.clientHeight;
 
 		const sidebar = leftPanel.parentElement.previousSibling.previousSibling;
 		const settingsButton = sidebar.querySelector("[aria-label='Settings']").parentElement;
@@ -197,8 +235,40 @@ async function run() {
 		gameControlButton.id = "wac-control-button";
 	}
 
-	// starting the game
-	setupGameArea();
+	function setupGameArea() {
+		const leftPanel = document.getElementById("side");
+		const rightPanel = leftPanel.parentElement.nextSibling;
+		const bannerDiv = rightPanel.querySelector("div");
+		bannerDiv.id = "wa-banner-div";
+
+		for (const child of rightPanel.children) {
+			if (child.id === bannerDiv.id) {
+				continue;
+			}
+			rightPanel.removeChild(child);
+		}
+
+		const canvas = document.createElement("canvas");
+		rightPanel.appendChild(canvas);
+
+		canvas.style.visibility = "hidden";
+		canvas.id = "game-canvas";
+		canvas.style.position = "fixed";
+		canvas.style.width = "100%";
+		canvas.style.height = "100%";
+		canvas.style.backgroundColor = CANVAS_BACKGROUND_COLOR;
+		canvas.width = canvas.clientWidth;
+		canvas.height = canvas.clientHeight;
+	}
+
+	setupControlButtons();
+
+	function setCanvasVisible(visible) {
+		const canvas = document.getElementById("game-canvas");
+		const bannerDiv = document.getElementById("wa-banner-div");
+		canvas.style.visibility = visible ? "visible" : "hidden";
+		bannerDiv.style.visibility = visible ? "hidden" : "visible";
+	}
 
 	const controlButton = document.getElementById("wac-control-button");
 	controlButton.firstChild.onclick = play;
@@ -209,32 +279,46 @@ async function run() {
 	}
 
 	function blockInputOutside(canvas) {
-		function stopEvent(e) {
-			if (controlButton.contains(e.target)) {
-				return;
-			}
-			
-			
-			if (!canvas.contains(e.target)) {
-				e.stopPropagation();
-				e.preventDefault();
-			}
-		}
-
-		['click', 'mousedown', 'mouseup', 'contextmenu'].forEach(evt =>
-			document.addEventListener(evt, stopEvent, true)
-		);
-		
 		function resolveKey(key) {
 			if (key === " ") {
 				return "Space";
 			}
 			return key;
 		}
-		
+
+		function resolveMouseClick(buttons) {
+			switch (buttons) {
+				case 1:
+					return "LMB";
+				case 2:
+					return "RMB";
+				case 4:
+					return "MMB";
+			}
+		}
+
+		const mousedown = (e) => {
+			const canvas = document.getElementById("game-canvas");
+			const leftWidth = document.body.clientWidth - canvas.width;
+			const leftHeight = document.body.clientHeight - canvas.height;
+			const key = resolveMouseClick(e.buttons);
+			currentMouse = key;
+			currentMouseX = e.clientX - leftWidth;
+			currentMouseY = e.clientY - leftHeight;
+			e.stopPropagation();
+			e.preventDefault();
+		};
+		document.addEventListener("mousedown", mousedown, true);
+		const mouseup = (e) => {
+			currentMouse = null;
+			currentMouseX = null;
+			currentMouseY = null;
+			e.stopPropagation();
+			e.preventDefault();
+		}
+		document.addEventListener("mouseup", mouseup, true);
 		const keydown = (e) => {
 			const key = resolveKey(e.key);
-			console.info(key);
 			currentKey[key] = true;
 			currentKeyTime[key] = Date.now();
 			e.stopPropagation();
@@ -250,27 +334,19 @@ async function run() {
 		}
 		document.addEventListener("keyup", keyup, true);
 
-// 		['keydown', 'keyup', 'keypress'].forEach(evt =>
-// 			document.addEventListener(evt, stopEvent, true)
-// 		);
-
 		return function restoreInput() {
-			['click', 'mousedown', 'mouseup', 'contextmenu'].forEach(evt =>
-				document.removeEventListener(evt, stopEvent, true)
-			);
-// 			['keydown', 'keyup', 'keypress'].forEach(evt =>
-				document.removeEventListener("keydown", keydown, true);
-				document.removeEventListener("keyup", keyup, true);
-// 			);
+			document.removeEventListener("mousedown", mousedown, true);
+			document.removeEventListener("mouseup", mouseup, true);
+			document.removeEventListener("keydown", keydown, true);
+			document.removeEventListener("keyup", keyup, true);
 		};
 	}
-
 
 	function initialize() {
 		let lastTimestamp = null;
 		let frames = 0;
 		let secondProgress = 0;
-
+		
 		const canvas = document.getElementById("game-canvas");
 		canvas.focus();
 		window.restoreListeners = blockInputOutside(canvas);
@@ -280,30 +356,224 @@ async function run() {
 			.filter((chat) => {
 				if (chat.pp == null || chat.pp.preview == null) return false;
 				return chat.name != null || (chat.contact != null && (chat.contact.name != null || chat.contact.shortName != null || chat.contact.pushname != null));
-			})
-		const randomlySelectedChat = chatsWithProfile[Math.floor(Math.random() * chatsWithProfile.length)];
+			});
 
-		const actor = new ChatActor(randomlySelectedChat);
-		stage.addActor(actor);
+		const MAX_ENEMIES_IN_A_WAVE = 5;
+		const MIN_SPAWN_COOLDOWN = 3;
+		const MAX_SPAWN_COOLDOWN = 10;
+
+		function randomFloat(min, max) {
+			return Math.random() * (max - min + 1) + min;
+		}
+
+		function randomInt(min, max) {
+			return Math.floor(randomFloat(min, max));
+		}
+
+		const GRAVITY = 200;
 
 		class Hero extends ChatActor {
 			me;
-			v = 300;
+			vx = 300;
+			vy = 0;
+
+			onGround = false;
+			groundY = 831;
 
 			constructor() {
 				super(ME);
 			}
 
 			act(delta) {
+				if (currentMouse === "LMB") {
+					const bullet = new TextBullet();
+					bullet.setPosition(this.x + this.width / 2, this.y + this.height / 2);
+					bullet.setSpawn(this.x + this.width / 2, this.y + this.height / 2);
+					bullet.setDestination(currentMouseX, currentMouseY);
+
+					this.stage.addActor(bullet);
+
+					const angle = Math.atan((currentMouseY - this.y) / (currentMouseX - this.x));
+
+					currentMouse = null;
+					currentMouseX = null;
+					currentMouseY = null;
+				}
+
 				if (currentKey.ArrowRight) {
-					this.x += delta * this.v;
+					this.x += delta * this.vx;
 				}
 				if (currentKey.ArrowLeft) {
-					this.x -= delta * this.v;
+					this.x -= delta * this.vx;
 				}
-				if (currentKey.Space) {
-					this.y -= delta * this.v;
+				if (currentKey.Space && this.onGround) {
+					this.vy = -200;
+					this.onGround = false;
 				}
+
+				this.vy += GRAVITY * delta;
+				this.y += this.vy * delta;
+
+				if (this.y >= this.groundY) {
+					this.y = this.groundY;
+					this.vy = 0;
+					this.onGround = true;
+				}
+			}
+		}
+
+		class EnemySystem extends Actor {
+			enemyChats = [];
+			currentActiveEnemies = 0;
+			spawnCooldown = 0;
+
+			constructor(enemyChats) {
+				super();
+
+				this.enemyChats = enemyChats;
+			}
+
+			spawnEnemy() {
+				if (this.enemyChats.length === 0) return;
+
+				const randomChat = this.enemyChats[Math.floor(Math.random() * this.enemyChats.length)];
+				const enemy = new Enemy(randomChat);
+				stage.addActor(enemy);
+				enemy.setPosition(
+					randomInt(0, stage.width - enemy.width),
+					randomInt(0, stage.height - enemy.height)
+				);
+				this.currentActiveEnemies++;
+				this.spawnCooldown = randomFloat(MIN_SPAWN_COOLDOWN, MAX_SPAWN_COOLDOWN);
+			}
+
+			act(delta) {
+				super.act(delta);
+
+				if (this.enemyChats.length === 0) return;
+
+				if (this.currentActiveEnemies >= MAX_ENEMIES_IN_A_WAVE) {
+					return;
+				}
+
+				this.spawnCooldown -= delta;
+				if (this.spawnCooldown <= 0) {
+					this.spawnEnemy();
+				}
+			}
+		}
+
+		const enemySystem = new EnemySystem(chatsWithProfile);
+		stage.addActor(enemySystem);
+
+		class Enemy extends ChatActor {
+			vx = 200;
+			vy = 400;
+
+			constructor(chat) {
+				super(chat);
+			}
+
+			act(delta) {
+				super.act(delta);
+				this.x += this.vx * delta;
+				this.y += this.vy * delta;
+
+				if (this.x <= 0 && this.vx < 0) {
+					this.vx *= -1;
+				}
+				if (this.x + CHARACTER_SIZE >= this.stage.width && this.vx >= 0) {
+					this.vx *= -1;
+				}
+
+				if (this.y <= 0 && this.vy < 0) {
+					this.vy *= -1;
+				}
+				if (this.y + CHARACTER_SIZE >= this.stage.height && this.vy >= 0) {
+					this.vy *= -1;
+				}
+			}
+		}
+
+		class TextBullet extends Actor {
+			speed = 600;
+
+			text = "o";
+
+			spawnX = 0;
+			spawnY = 0;
+			destX = 0;
+			destY = 0;
+
+			constructor() {
+				super();
+
+				this.setText("o");
+			}
+
+			setSpawn(x, y) {
+				this.spawnX = x;
+				this.spawnY = y;
+			}
+
+			setDestination(x, y) {
+				this.destX = x;
+				this.destY = y;
+			}
+
+			setText(text) {
+				this.text = text;
+			}
+
+			act(delta) {
+				super.act(delta);
+
+				const dx = this.destX - this.spawnX;
+				const dy = this.destY - this.spawnY;
+				const len = Math.sqrt(dx * dx + dy * dy);
+				const ux = dx / len;
+				const uy = dy / len;
+				const vx = ux * this.speed;
+				const vy = uy * this.speed;
+
+				this.x += vx * delta;
+				this.y += vy * delta;
+
+				const ctx = this.stage.ctx;
+				ctx.font = "16px 'Arial'";
+				ctx.textAlign = "center";
+				ctx.fillStyle = "yellow";
+				const metrics = ctx.measureText(this.text);
+				this.width = metrics.width;
+				this.height = metrics.actualBoundingBoxAscent + metrics.actualBoundingBoxDescent;
+
+				for (const actor of this.stage.getActors()) {
+					if (actor instanceof Enemy) {
+						const target = actor;
+
+						if (
+							(this.x >= target.x && this.x <= target.x + target.width) &&
+							(this.y >= target.y && this.y <= target.y + target.height)
+						) {
+							this.stage.removeActor(target);
+						}
+					}
+				}
+
+				if (this.x < 0 || this.x > this.stage.width || this.y < 0 || this.y > this.stage.height) {
+					this.stage.removeActor(this);
+				}
+			}
+
+			draw() {
+				super.draw();
+
+				const ctx = this.stage.ctx;
+
+				ctx.font = "16px 'Arial'";
+				ctx.textAlign = "center";
+				ctx.fillStyle = "yellow";
+				ctx.fillText(this.text, this.x, this.y);
 			}
 		}
 
@@ -334,10 +604,12 @@ async function run() {
 	}
 
 	function play() {
+		setupGameArea();
 		window.gameCurrentState = GAME_STATES.LOADING_DATA;
 		console.info("LOADING_DATA");
+		setCanvasVisible(true);
 		changeControlButtonIcon(SVG_ICONS.loaderCircle);
-		controlButton.style.animation = "spin 2s linear infinite";
+		// todo: controlButton.style.animation = "spin 2s linear infinite";
 		controlButton.firstChild.onclick = null;
 
 		findRequiredWhatsappData()
@@ -375,10 +647,14 @@ async function run() {
 		if (window.restoreListeners) {
 			window.restoreListeners();
 		}
+		setCanvasVisible(false);
 		window.gameCurrentState = GAME_STATES.STOPPED;
 		console.info("STOPPING");
 		controlButton.firstChild.onclick = play;
 		changeControlButtonIcon(SVG_ICONS.play);
+		const canvas = document.getElementById("game-canvas");
+		const ctx = canvas.getContext("2d");
+		ctx.clearRect(0, 0, canvas.width, canvas.height);
 
 		CHATS = null;
 		ME = null;
@@ -535,7 +811,6 @@ function getIndexedDbData() {
 			// 					}
 			// 				}
 			// 			}
-
 
 			resolve({
 				chats,
